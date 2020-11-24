aa68ba9b0d4f20f669d51f7dc55233fbdeb1d0ff
Vuprazz <74353875+Vuprazz@users.noreply.github.com>
2020-11-24 13:53:19 +0100

    Update pong_class.py

--- pong_class.py
+++ pong_class.py
@@ -2,8 +2,6 @@ class Raquettes:
     
     def __init__(self, direction):
         self._direction = direction
-        self.bx = 1
-        self.by = 1
     
     def set_direction(self,val):
         self._direction = val
