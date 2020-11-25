8719d8296f7ffcd3477bcd9b8b3f17ecb1ee1a7c
Skyflooze <74588051+Skyflooze@users.noreply.github.com>
2020-11-25 09:51:16 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -6,16 +6,28 @@ largeur = 500
 hauteur = 300
 
 def raquette1_haut(event):
-    canvas.move(raquette1_canvas,0,-5)
+    if (canvas.coords(raquette1_canvas)[1]<=0):
+        canvas.move(raquette1_canvas,0,0)
+    else:
+        canvas.move(raquette1_canvas,0,-5)
 
 def raquette1_bas(event):
-    canvas.move(raquette1_canvas,0,5)
+    if (canvas.coords(raquette1_canvas)[1]>=220):
+        canvas.move(raquette1_canvas,0,0)
+    else:
+        canvas.move(raquette1_canvas,0,5)
 
 def raquette2_haut(event):
-    canvas.move(raquette2_canvas,0,-5)
+    if (canvas.coords(raquette2_canvas)[1]<=0):
+        canvas.move(raquette2_canvas,0,0)
+    else:
+        canvas.move(raquette2_canvas,0,-5)
 
 def raquette2_bas(event):
-    canvas.move(raquette2_canvas,0,5)
+    if (canvas.coords(raquette2_canvas)[1]>=220):
+        canvas.move(raquette2_canvas,0,0)
+    else:
+        canvas.move(raquette2_canvas,0,5)
 
 
 def balle_move():
@@ -36,7 +48,7 @@ def balle_move():
 
     canvas.move(balle1,raq.bx,raq.by)
     root.after(20,balle_move)
-    
+
 
 def est_mort():
     canvas.delete(ALL)
@@ -49,8 +61,8 @@ text = Text(root)
 raq = Raquettes()
 
 canvas = Canvas(root, width=largeur, height=hauteur, background="black")
-raquette1_canvas = canvas.create_rectangle(6, 125,  11, 175, width=1, fill="white", outline="")
-raquette2_canvas = canvas.create_rectangle(497, 125, 492, 175, width=1, fill="white", outline="")
+raquette1_canvas = canvas.create_rectangle(6, 125,  11, 200, width=1, fill="white", outline="")
+raquette2_canvas = canvas.create_rectangle(497, 125, 492, 200, width=1, fill="white", outline="")
 balle1 = canvas.create_oval(240,120,260,140,fill='red')
 bord_haut = canvas.create_rectangle(0, 0, 503, 10, fill='green')
 bord_bas = canvas.create_rectangle(0, 292, 503, 302, fill='green')
