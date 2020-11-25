34b2b463430b23bf9fcaeb5695c0241b3a4e555b
Skyflooze <74588051+Skyflooze@users.noreply.github.com>
2020-11-25 08:58:17 +0100

    Update pong_class.py

--- pong_class.py
+++ pong_class.py
@@ -3,6 +3,8 @@ class Raquettes:
     def __init__(self):
         self.bx = 4
         self.by = 4
+        self.scorej1 = 0
+        self.scorej2 = 0
     
     def set_direction(self,val):
         self._direction = val
